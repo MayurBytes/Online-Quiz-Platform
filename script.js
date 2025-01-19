bb037a791628285903ceda96560a554a2b89b9b3
Mayur Deshmukh <deshmukhmayur454@gmail.com>
2025-01-19 11:31:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+const questions = [
+    {
+        question: "What is the capital city of France?",
+        answers: [
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Rome", correct: false },
+        ]
+    },
+    {
+        question: "How many continents are there in the world?",
+        answers: [
+            { text: "5", correct: false },
+            { text: "6", correct: false },
+            { text: "7", correct: true },
+            { text: "8", correct: false },
+        ]
+    },
+    {
+        question: 'Which planet is known as the "Red Planet"?',
+        answers: [
+            { text: "Earth", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Jupiter", correct: false },
+            { text: "Venus", correct: false },
+        ]
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        answers: [
+            { text: "Atlantic Ocean", correct: false },
+            { text: "Indian Ocean", correct: false },
+            { text: "Arctic Ocean", correct: false },
+            { text: "Pacific Ocean", correct: true },
+        ]
+    },
+    {
+        question: "Who wrote the play Romeo and Juliet?",
+        answers: [
+            { text: "Charles Dickens", correct: false },
+            { text: "J.K. Rowling", correct: false },
+            { text: "William Shakespeare", correct: true },
+            { text: "Mark Twain", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNO = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNO + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+    if (isCorrect) {
+        selectedButton.classList.add("correct");
+        score++;
+    } else {
+        selectedButton.classList.add("incorrect");
+    }
+
+    selectedButton.classList.add("clicked"); 
+
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+const buttons = document.querySelectorAll('.btn');
+const correctAnswerIndex = 1;
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+       
+        buttons.forEach(btn => {
+            btn.classList.remove('correct', 'wrong');
+        });
+
+        if (index === correctAnswerIndex) {
+            button.classList.add('correct'); 
+        } else {
+            button.classList.add('wrong'); 
+        }
+    });
+});
+
+
+startQuiz();
